d72025724e3457808141f0d5a58260894db45c07
Gabriel Vasconcelos <gfreivasc@gmail.com>
2016-06-27 17:09:52 -0300

    daptando url resolvers para labels alfanuméricas

--- malha/routers.py
+++ malha/routers.py
@@ -4,7 +4,7 @@ from django.conf.urls import url
 from malha.views import *
 
 urlpatterns = [
-    url(r'^linha/(?P<label>\d+)/$', linha_view, name='linha'),
-    url(r'^parada/(?P<label>\d+)/$', parada_view, name='parada'),
+    url(r'^linha/(?P<label>\w+)/$', linha_view, name='linha'),
+    url(r'^parada/(?P<label>\w+)/$', parada_view, name='parada'),
     url(r'^paradas/$', parada_listview, name='paradas')
 ]
